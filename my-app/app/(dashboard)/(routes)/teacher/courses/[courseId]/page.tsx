36697204914b79c3652687f8e93fd7bd3a08a329
Sudha9524 <sudha8161161@gmail.com>
2025-08-20 20:03:46 +0530

    title and description form

--- my-app/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ my-app/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,15 +1,17 @@
-import { auth } from "@clerk/nextjs"; 
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { LayoutDashboard } from "lucide-react";
 import { IconBadge } from "@/app/navbarComponents/icon-badge";
+import { auth } from "@clerk/nextjs/server";
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({
   params,
 }: {
   params: { courseId: string };
 }) => {
-  const { userId } = auth();
+  const { userId } = await auth();
 
   if (!userId) {
     return redirect("/"); 
@@ -53,6 +55,14 @@ const CourseIdPage = async ({
             <IconBadge icon={LayoutDashboard}/>
             <h2 className="text-xl">Customize your course</h2>
           </div>
+          <TitleForm
+          initialData={course}
+          courseId={course.id}
+          />
+          <DescriptionForm
+          initialData={course}
+          courseId={course.id}
+          />
         </div>
       </div>
     </div>
